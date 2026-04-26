ddbdc720c884191a27ab2d16fded443a00cfc3e9
Pedro Miguel <opedromiguel.sc@gmail.com>
2026-04-26 14:15:01 -0300

    Add files via upload

---
+++ incs/configs/configs.php
@@ -0,0 +1,8 @@
+<?php
+
+    define("HOST_DB", "127.0.0.1");
+    define("BANCO_DE_DADOS", "siteQUIZ");
+    define("USUARIO_DB", "adminsiteQUIZ");
+    define("SENHA_DB", "1QAZXSW23EDCvcxzasdfrewq1234");
+
+?>
